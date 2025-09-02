129b02976665d75b47111e72ca6b9ed31c4a8585
Prakrit M. <Prakritmanstudio@gmail.com>
2025-09-02 14:39:37 +0700

    chore: update version to 1.0.0-beta.4 in package.json and add Leave message type in messages.ts

--- src/type/messages.ts
+++ src/type/messages.ts
@@ -17,105 +17,105 @@ export type Message = {
     userId: string;
   };
   message:
-    | {
-        id: string;
-        type: "location";
-        latitude: number;
-        longitude: number;
-        address: string;
-      }
-    | {
-        id: string;
-        type: "audio";
-        contentProvider: {
-          type: "line";
-          contentHash: string;
-          expired: boolean;
-          expiredAt: number;
-        };
-        duration: number;
-        contentHash: string;
-        expired: boolean;
-        expiredAt: number;
-      }
-    | {
-        id: string;
-        type: "image";
-        quoteToken?: string;
-        contentProvider: {
-          type: "line";
-          contentHash: string;
-          expired: boolean;
-          expiredAt: number;
-        };
-        contentHash: string;
-        expired: boolean;
-        expiredAt: number;
-      }
-    | {
-        id: string;
-        type: "video";
-        quoteToken?: string;
-        contentProvider: {
-          type: "line";
-          contentHash: string;
-          expired: boolean;
-          expiredAt: number;
-        };
-        duration: number;
-        contentHash: string;
-        expired: boolean;
-        expiredAt: number;
-      }
-    | {
-        id: string;
-        type: "unsent";
-      }
-    | {
-        id: string;
-        type: "file";
-        contentProvider: {
-          type: "line";
-          contentHash: string;
-          expired: boolean;
-          expiredAt: number;
-        };
-        fileName: string;
-        fileSize: number;
-        contentHash: string;
-        expired: boolean;
-        expiredAt: number;
-      }
-    | {
-        id: string;
-        quotedMessageId?: string;
-        quoteToken?: string;
-        packageId: string;
-        stickerId: string;
-        stickerResourceType: "STATIC" | "ANIMATION_SOUND";
-        type: "sticker";
-      }
-    | {
-        id: string;
-        type: "text";
-        quotedMessageId?: string;
-        quoteToken?: string;
-        text: string;
-        emojis?: {
-          index: number;
-          length: number;
-          productId: string;
-          emojiId: string;
-        }[];
-        mention?: {
-          mentionees: {
-            index: number;
-            length: number;
-            userId: string;
-            type: "user";
-          }[];
-        };
-      };
+  | {
+    id: string;
+    type: "location";
+    latitude: number;
+    longitude: number;
+    address: string;
+  }
+  | {
+    id: string;
+    type: "audio";
+    contentProvider: {
+      type: "line";
+      contentHash: string;
+      expired: boolean;
+      expiredAt: number;
+    };
+    duration: number;
+    contentHash: string;
+    expired: boolean;
+    expiredAt: number;
+  }
+  | {
+    id: string;
+    type: "image";
+    quoteToken?: string;
+    contentProvider: {
+      type: "line";
+      contentHash: string;
+      expired: boolean;
+      expiredAt: number;
+    };
+    contentHash: string;
+    expired: boolean;
+    expiredAt: number;
+  }
+  | {
+    id: string;
+    type: "video";
+    quoteToken?: string;
+    contentProvider: {
+      type: "line";
+      contentHash: string;
+      expired: boolean;
+      expiredAt: number;
+    };
+    duration: number;
+    contentHash: string;
+    expired: boolean;
+    expiredAt: number;
+  }
+  | {
+    id: string;
+    type: "unsent";
+  }
+  | {
+    id: string;
+    type: "file";
+    contentProvider: {
+      type: "line";
+      contentHash: string;
+      expired: boolean;
+      expiredAt: number;
+    };
+    fileName: string;
+    fileSize: number;
+    contentHash: string;
+    expired: boolean;
+    expiredAt: number;
+  }
+  | {
+    id: string;
+    quotedMessageId?: string;
+    quoteToken?: string;
+    packageId: string;
+    stickerId: string;
+    stickerResourceType: "STATIC" | "ANIMATION_SOUND";
+    type: "sticker";
+  }
+  | {
+    id: string;
+    type: "text";
+    quotedMessageId?: string;
+    quoteToken?: string;
+    text: string;
+    emojis?: {
+      index: number;
+      length: number;
+      productId: string;
+      emojiId: string;
+    }[];
+    mention?: {
+      mentionees: {
+        index: number;
+        length: number;
+        userId: string;
+        type: "user";
+      }[];
+    };
+  };
 };
 
 type MessageSent = {
@@ -126,71 +126,71 @@ type MessageSent = {
     userId: string;
   };
   message:
-    | {
-        id: string;
-        type: "video";
-        quoteToken?: string;
-        contentProvider: {
-          type: "external";
-          originalContentUrl: string;
-          previewImageUrl: string;
-        };
-        duration: number;
-      }
-    | {
-        id: string;
-        type: "image";
-        quoteToken?: string;
-        contentProvider: {
-          type: "external";
-          originalContentUrl: string;
-          previewImageUrl: string;
-        };
-      }
-    | {
-        id: string;
-        type: "text";
-        quoteToken?: string;
-        text: string;
-        emojis?: {
-          index: number;
-          length: number;
-          productId: string;
-          emojiId: string;
-        }[];
-        mention?: {
-          mentionees: {
-            index: number;
-            length: number;
-            userId: string;
-            type: "user";
-          }[];
-        };
-        quotedMessageId?: string;
-      }
-    | {
-        id: string;
-        quoteToken?: string;
-        packageId: string;
-        stickerId: string;
-        stickerResourceType: "ANIMATION_SOUND";
-        type: "sticker";
-      }
-    | {
-        id: string;
-        type: "file";
-        contentProvider: {
-          type: "line";
-          contentHash: string;
-          expired: boolean;
-          expiredAt: number;
-        };
-        fileName: string;
-        fileSize: number;
-        contentHash: string;
-        expired: boolean;
-        expiredAt: number;
-      };
+  | {
+    id: string;
+    type: "video";
+    quoteToken?: string;
+    contentProvider: {
+      type: "external";
+      originalContentUrl: string;
+      previewImageUrl: string;
+    };
+    duration: number;
+  }
+  | {
+    id: string;
+    type: "image";
+    quoteToken?: string;
+    contentProvider: {
+      type: "external";
+      originalContentUrl: string;
+      previewImageUrl: string;
+    };
+  }
+  | {
+    id: string;
+    type: "text";
+    quoteToken?: string;
+    text: string;
+    emojis?: {
+      index: number;
+      length: number;
+      productId: string;
+      emojiId: string;
+    }[];
+    mention?: {
+      mentionees: {
+        index: number;
+        length: number;
+        userId: string;
+        type: "user";
+      }[];
+    };
+    quotedMessageId?: string;
+  }
+  | {
+    id: string;
+    quoteToken?: string;
+    packageId: string;
+    stickerId: string;
+    stickerResourceType: "ANIMATION_SOUND";
+    type: "sticker";
+  }
+  | {
+    id: string;
+    type: "file";
+    contentProvider: {
+      type: "line";
+      contentHash: string;
+      expired: boolean;
+      expiredAt: number;
+    };
+    fileName: string;
+    fileSize: number;
+    contentHash: string;
+    expired: boolean;
+    expiredAt: number;
+  };
   sendId?: string;
   bizId?: string;
 };
@@ -208,6 +208,14 @@ type Unsend = {
 
 type Join = {
   type: "join";
+  timestamp: number;
+  source: {
+    chatId: string;
+  };
+};
+
+type Leave = {
+  type: "leave";
   timestamp: number;
   source: {
     chatId: string;
@@ -246,6 +254,7 @@ export type MessageEventType =
   | MessageSent
   | Unsend
   | Join
+  | Leave
   | MemberLeft
   | MemberJoined;
 
